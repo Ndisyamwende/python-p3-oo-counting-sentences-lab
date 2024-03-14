9c33d653131c4f55d79b366ae7dfc20344370d1f
Ndisyamwende <sharonndisya2002@gmail.com>
2024-03-14 13:11:31 +0300

    passed tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+            self.value = ''
+        else:
+            self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        # Use regular expression to split the string into sentences
+        sentences = re.split(r'[.!?]', self.value)
+        # Count the number of non-empty sentences
+        count = sum(1 for sentence in sentences if sentence.strip())
+        return count
